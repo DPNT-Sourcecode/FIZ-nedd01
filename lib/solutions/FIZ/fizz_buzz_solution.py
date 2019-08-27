987439e9c555adde359d33bec1fb850d0ae81af8
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 15:51:21 +0000

    Tue Aug 27 15:51:21 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,17 +1,6 @@
 # noinspection PyUnusedLocal
 
-def identical(num):
-    l = len(num)
-    f = num[0]
-    c = 0
-    while (c<=l-1):
-        if (num[c] == f):
-            if (c <= l - 1):
-                return True
-            c+=1
 
-        else:
-            return  False
 
 
 
@@ -20,7 +9,21 @@ def identical(num):
 def fizz_buzz(number):
     # if number %3 ==0 and number%5 ==0:
     #     return ('fizz buzz')
-    if (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number)) and (number >10 and identfical(str(number))):
+
+    def identical(num):
+        l = len(num)
+        f = num[0]
+        c = 0
+        while (c <= l - 1):
+            if (num[c] == f):
+                if (c <= l - 1):
+                    return True
+                c += 1
+
+            else:
+                return False
+
+    if (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number)) and (number >10 and identical(str(number))):
         return ('fizz buzz deluxe')
     elif  (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number))  :
         return ('fizz buzz')
@@ -28,11 +31,7 @@ def fizz_buzz(number):
         return ('fizz')
     elif number %5 ==0 or '5' in str (number) :
         return ('buzz')
-    elif (number >10 and identfical(str(number))):
+    elif (number >10 and identical(str(number))):
         return ('deluxe')
     else:
         return number
-
-
-
-
