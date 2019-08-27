322567e78a0185db9b5343a05a7b084e0622de7c
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 15:41:32 +0000

    Tue Aug 27 15:41:32 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,4 +1,20 @@
 # noinspection PyUnusedLocal
+
+def identical(num):
+    l = len(num)
+    c = 0
+    while (c<=l-1):
+        if (num[c] == num[c+1]):
+            pass
+        elif (c<=l-1 and num[c] == num[c+1]):
+            return True
+        else:
+            return  False
+        c+=1
+        
+
+
+
 def fizz_buzz(number):
     # if number %3 ==0 and number%5 ==0:
     #     return ('fizz buzz')
@@ -8,8 +24,9 @@ def fizz_buzz(number):
         return ('fizz')
     elif number %5 ==0 or '5' in str (number) :
         return ('buzz')
-    elif (number >10 and identfical(number)):
+    elif (number >10 and identfical(str(number))):
         return ('deluxe')
     else:
         return number
+
 
