696f1ac54798758ff32d5912c2bb1b37aa09ca66
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 14:35:03 +0000

    Tue Aug 27 14:35:03 UTC 2019

--- lib/solutions/SUM/Sum_R1.py
+++ lib/solutions/SUM/Sum_R1.py
@@ -7,5 +7,9 @@ def sum (num1, num2):
 num1 = int(input('Please enter the first number between 0 to 100'))
 num2 = int(input('Please enter the second number between 0 to 100'))
 
-total = sum(num1, num2)
-print (total)
+if num1 >=0 and num1<= 100 and num2 >=0 and num2 <=100:
+    total = sum(num1, num2)
+    print (total)
+else:
+    print ('Entered numbers not in range')
+
