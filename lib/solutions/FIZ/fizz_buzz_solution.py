a7cd61449870cd23b50f91d171be4bc496dc844e
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 15:12:55 +0000

    Tue Aug 27 15:12:55 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,11 +1,14 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if number %3 ==0 or number%5 ==0 or ('5' in str(number) and '3' in str(number) ):
+    if number %3 ==0 and number%5 ==0:
         return ('fizz buzz')
     elif number %3==0 or '3' in str(number):
         return ('fizz')
     elif number %5 ==0 or '5' in str (number) :
         return ('buzz')
+    elif   ('5' in str(number) and '3' in str(number) ):
+        return ('fizz buzz')
     else:
         return number
+
 
