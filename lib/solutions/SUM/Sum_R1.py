8546bc03dcd5e392423a0d58162beb6348c4e71a
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 14:07:34 +0000

    Tue Aug 27 14:07:34 UTC 2019

--- lib/solutions/SUM/Sum_R1.py
+++ lib/solutions/SUM/Sum_R1.py
@@ -1,6 +1,6 @@
 
 def sum (num1, num2):
-    return print (f'Total of the two numbers: {num1+num2}')
+    return (num1+num2)
 
 
 
@@ -8,4 +8,5 @@ num1 = int(input('Please enter the first number between 0 to 100'))
 num2 = int(input('Please enter the second number between 0 to 100'))
 
 total = sum(num1, num2)
+
 
