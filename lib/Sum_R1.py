8134a7e394b3d06289da3d2758c3fcb80d688ec4
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 13:52:17 +0000

    Tue Aug 27 13:52:17 UTC 2019

---
+++ lib/Sum_R1.py
@@ -0,0 +1,11 @@
+
+def sum (num1, num2):
+    return print ('Total of the two numbers: '+ num1+num2)
+
+
+
+num1 = int(input('Please enter the first number between 0 to 100'))
+num2 = int(input('Please enter the second number between 0 to 100'))
+
+total = sum(num1, num2)
+
