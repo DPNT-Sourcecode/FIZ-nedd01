a0fd40a056d5d970f05e18af67179ebdd7d90283
sbx_user1059 <sbx_user1059@ip-10-129-78-208.eu-west-2.compute.internal>
2019-08-27 15:44:32 +0000

    Tue Aug 27 15:44:32 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -11,15 +11,17 @@ def identical(num):
         else:
             return  False
         c+=1
-        
+
 
 
 
 def fizz_buzz(number):
     # if number %3 ==0 and number%5 ==0:
     #     return ('fizz buzz')
-    if  (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number))  :
-        return ('fizz buzz')
+    if (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number)) and (number >10 and identfical(str(number))):
+        return ('fizz buzz deluxe')
+    elif  (number %3==0 or '3' in str(number)) and (number %5 ==0 or '5' in str (number))  :
+        return ('fizz buzz')0
     elif number %3==0 or '3' in str(number):
         return ('fizz')
     elif number %5 ==0 or '5' in str (number) :
@@ -28,5 +30,6 @@ def fizz_buzz(number):
         return ('deluxe')
     else:
         return number
+
 
 
